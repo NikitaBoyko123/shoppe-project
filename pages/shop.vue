ef59521b44b9d6bb73ec057453d7397acc95b3ce
NikitaBoyko123 <nikitaboyko1911@mail.ru>
2025-12-17 03:57:12 +0300

    fix
    add about.vue blog.vue and shop.vue to fix error with build

---
+++ pages/shop.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="page">
+    <section class="page__section">
+      <h1 class="page__title">Shop</h1>
+      <p class="page__lead">Our products will be available here soon.</p>
+      <NuxtLink to="/" class="page__link">Back to home</NuxtLink>
+    </section>
+  </main>
+</template>
